27636aedf981d56210310fcda10751fa8fe3d994
Varis Darasirikul <varis@varidentech.com>
2026-09-12 10:33:01 +0700

    Lesson 6: Create a basic CoffeesController
    nest g co coffees, GET /coffees returns a placeholder string.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { CoffeesController } from './coffees/coffees.controller.js';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, CoffeesController],
   providers: [AppService],
 })
 export class AppModule {}
