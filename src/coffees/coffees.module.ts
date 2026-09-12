18359cfeaf5d45293c76a477dee16535bebfbf6b
Varis Darasirikul <manjarb4@gmail.com>
2026-09-13 02:56:46 +0700

    Lesson 33: Diving into custom providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -4,7 +4,7 @@ import { CoffeesService } from './coffees.service.js';
 
 @Module({
   controllers: [CoffeesController],
-  providers: [CoffeesService],
+  providers: [{ provide: CoffeesService, useClass: CoffeesService }],
   exports: [CoffeesService],
 })
 export class CoffeesModule {}
