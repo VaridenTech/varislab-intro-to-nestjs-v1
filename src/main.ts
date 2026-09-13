ed9df396aee565f4d8cbeb871d203ea6eabc4238
Varis Darasirikul <manjarb4@gmail.com>
2026-09-13 13:36:36 +0700

    Lesson 52: Protect routes with guards

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter.js';
+import { ApiKeyGuard } from './common/guards/api-key.guard.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalGuards(new ApiKeyGuard());
   await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
