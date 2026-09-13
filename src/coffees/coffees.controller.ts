dbfddf1f76dac35de4de770866fda09faa32297a
Varis Darasirikul <manjarb4@gmail.com>
2026-09-13 13:40:41 +0700

    Lesson 56: Creating custom pipes

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -13,6 +13,7 @@ import { CreateCoffeeDto } from './dto/create-coffee.dto.js';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto.js';
 import { PaginationQueryDto } from '../common/dto/pagination-query.dto.js';
 import { Public } from '../common/decorators/public.decorator.js';
+import { ParseIntPipe } from '../common/pipes/parse-int.pipe.js';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -25,8 +26,8 @@ export class CoffeesController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.coffeesService.findOne(id);
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.coffeesService.findOne('' + id);
   }
 
   @Post()
