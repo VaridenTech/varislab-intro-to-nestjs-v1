2ae6f83594ed921e9e8cd598764529ee25cbe485
Varis Darasirikul <manjarb4@gmail.com>
2026-09-13 13:38:36 +0700

    Lesson 54: Add pointcuts with interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter.js';
+import { WrapResponseInterceptor } from './common/interceptors/wrap-response.interceptor.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new WrapResponseInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
