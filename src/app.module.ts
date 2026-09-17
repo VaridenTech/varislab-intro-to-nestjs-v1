0a4a0a4d4c05eef60f1abc8fbdf84dabc11099c6
Varis Darasirikul <manjarb4@gmail.com>
2026-09-17 16:48:15 +0700

    Lesson 12: Create a basic CoffeesService

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { CoffeesController } from './coffees/coffees.controller.js';
+import { CoffeesService } from './coffees/coffees.service.js';
 
 @Module({
   imports: [],
   controllers: [AppController, CoffeesController],
-  providers: [AppService],
+  providers: [AppService, CoffeesService],
 })
 export class AppModule {}
