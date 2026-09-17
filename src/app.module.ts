68635ab083310f80acb0943ff50cefaf95c841ab
Varis Darasirikul <manjarb4@gmail.com>
2026-09-17 16:59:34 +0700

    Lesson 22: Introduce Prisma with CLI as a development dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { CoffeesModule } from './coffees/coffees.module.js';
+import { PrismaModule } from './prisma/prisma.module.js';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [CoffeesModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
