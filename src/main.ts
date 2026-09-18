5a17c56375c3c7a78c717d639d213f378237746f
Varis Darasirikul <manjarb4@gmail.com>
2026-09-18 15:22:56 +0700

    Lesson 36: Explain request lifecycle and binding

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-      forbidNonWhitelisted: true,
-      transformOptions: {
-        enableImplicitConversion: true,
-      },
-    }),
-  );
   await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
