b046ccf73b6052afabb9cd387482b9b8d8a95146
Varis Darasirikul <manjarb4@gmail.com>
2026-09-18 17:41:37 +0700

    Lesson 53: Introduce the Mongoose module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { CoffeesModule } from './coffees/coffees.module.js';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-course'),
+    CoffeesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
