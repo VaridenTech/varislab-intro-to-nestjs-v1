d799b346d3b6f4eab36d740000c7ba1c5d569ffb
Varis Darasirikul <manjarb4@gmail.com>
2026-09-18 15:23:10 +0700

    Lesson 39: Transform responses and handle timeouts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter.js';
+import { WrapResponseInterceptor } from './common/interceptors/wrap-response.interceptor.js';
+import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new WrapResponseInterceptor(), new TimeoutInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
