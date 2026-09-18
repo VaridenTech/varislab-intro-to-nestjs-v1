98231e925b901433e64e3ae90d808a6fa21941b4
Varis Darasirikul <manjarb4@gmail.com>
2026-09-18 14:58:29 +0700

    Lesson 33: Configure ConfigModule and environment files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { CoffeesModule } from './coffees/coffees.module.js';
@@ -6,7 +7,12 @@ import { PrismaModule } from './prisma/prisma.module.js';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module.js';
 
 @Module({
-  imports: [CoffeesModule, PrismaModule, CoffeeRatingModule],
+  imports: [
+    ConfigModule.forRoot(),
+    CoffeesModule,
+    PrismaModule,
+    CoffeeRatingModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
