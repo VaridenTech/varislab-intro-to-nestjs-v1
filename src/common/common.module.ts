e123c50b9418312b0cb0fbf917e384f71e7923de
Varis Darasirikul <manjarb4@gmail.com>
2026-09-18 15:23:20 +0700

    Lesson 41: Add request logging middleware

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,8 +1,17 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+} from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { ApiKeyGuard } from './guards/api-key.guard.js';
+import { LoggingMiddleware } from './middleware/logging.middleware.js';
 
 @Module({
   providers: [{ provide: APP_GUARD, useClass: ApiKeyGuard }],
 })
-export class CommonModule {}
+export class CommonModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggingMiddleware).forRoutes('*');
+  }
+}
