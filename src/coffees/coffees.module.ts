e54c0ab922c9b1ef7ce66028427ab9a6bdd69ae4
Varis Darasirikul <manjarb4@gmail.com>
2026-09-18 17:41:42 +0700

    Lesson 54: Create a Mongoose model

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CoffeesController } from './coffees.controller.js';
 import { CoffeesService } from './coffees.service.js';
+import { Coffee, CoffeeSchema } from './entities/coffee.entity.js';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Coffee.name, schema: CoffeeSchema }]),
+  ],
   controllers: [CoffeesController],
   providers: [CoffeesService]
 })
